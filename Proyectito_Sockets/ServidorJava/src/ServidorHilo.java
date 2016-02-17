8a649b599deb8d992902207a915b7bb3c4c396da
lepe92 <eejl_1992@hotmail.com>
2016-02-16 18:51:39 -0600

    dos proyectos, respaldo

---
+++ Proyectito_Sockets/ServidorJava/src/ServidorHilo.java
@@ -0,0 +1,258 @@
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author edwin
+ */
+public class ServidorHilo extends Thread {
+    
+    private Socket socket;
+    private DataOutputStream dos;
+    private DataInputStream dis;
+    private int idSessio;
+    
+    public ServidorHilo(Socket socket) {
+        this.socket = socket;
+        
+        try {
+            dos = new DataOutputStream(socket.getOutputStream());
+            dis = new DataInputStream(socket.getInputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void desconnectar() {
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String actualizarNombre(String envio) {
+        String response = "";
+        OutputStream os = null;
+        InputStream is = null;
+        HttpURLConnection conn = null;
+        BufferedReader bufferedReader = null;
+        
+        try {
+            
+            String message = envio;
+            URL url = new URL("http://www.jimenezlepe.comuv.com/insertarnombre.php");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setFixedLengthStreamingMode(message.getBytes().length);
+            conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+            conn.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+
+            //open
+            conn.connect();
+
+                    //setup send
+            os = new BufferedOutputStream(conn.getOutputStream());
+            os.write(message.getBytes());
+            //clean up
+            os.flush();
+
+            //do somehting with response
+            is = conn.getInputStream();
+            bufferedReader = new BufferedReader(new InputStreamReader(is));
+
+//                    Log.i("mensaje2", macajena);
+            //Insercion correcta
+            String json;
+            while ((json = bufferedReader.readLine()) != null) {
+                // Log.i("mensaje", json);
+                System.out.println("Json " + json);
+                
+            }
+            response = "insertado";
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return response;
+    }
+    
+    public String solicitarJSON() {
+        
+        BufferedReader bufferedReader = null;
+        try {
+            URL url = new URL("http://jimenezlepe.comuv.com/solicitajson.php");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            StringBuilder sb = new StringBuilder();
+            
+            bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            
+            String json;
+            while ((json = bufferedReader.readLine()) != null) {
+                sb.append(json + "\n");
+            }
+            
+            return sb.toString().trim();
+            
+        } catch (Exception e) {
+            return null;
+        }
+        
+    }
+    
+    public String solicitarPersona(String message) {
+        OutputStream os = null;
+        BufferedReader bufferedReader = null;
+        try {
+            URL url = new URL("http://jimenezlepe.comuv.com/consultaubicacion.php");
+            
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con = (HttpURLConnection) url.openConnection();
+            con.setReadTimeout(10000);
+            con.setConnectTimeout(15000);
+            con.setRequestMethod("POST");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setFixedLengthStreamingMode(message.getBytes().length);
+
+            //make some HTTP header nicety
+            con.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+            con.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+
+            //open
+            con.connect();
+
+                    //setup send
+            os = new BufferedOutputStream(con.getOutputStream());
+            os.write(message.getBytes());
+            //clean up
+            os.flush();
+            
+            StringBuilder sb = new StringBuilder();
+            
+            bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            
+            String json;
+            while ((json = bufferedReader.readLine()) != null) {
+                sb.append(json + "\n");
+            }
+            
+            return sb.toString().trim();
+            
+        } catch (Exception e) {
+            return null;
+        }
+        
+    }
+        //solicitar persona "http://jimenezlepe.comuv.com/consultaubicacion.php";
+    
+    public String insertar(String cad) throws MalformedURLException, IOException {
+        OutputStream os = null;
+        InputStream is = null;
+        HttpURLConnection conn = null;
+        BufferedReader bufferedReader = null;
+        URL url = new URL("http://jimenezlepe.comuv.com/solicita.php");
+        
+        conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000);
+        conn.setConnectTimeout(15000);
+        conn.setRequestMethod("POST");
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setFixedLengthStreamingMode(cad.getBytes().length);
+        
+        conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+        conn.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+
+        //open
+        conn.connect();
+
+                    //setup send
+        os = new BufferedOutputStream(conn.getOutputStream());
+        os.write(cad.getBytes());
+        //clean up
+        os.flush();
+
+        //do somehting with response
+        is = conn.getInputStream();
+        bufferedReader = new BufferedReader(new InputStreamReader(is));
+        
+        String response = bufferedReader.readLine();
+        System.out.println(response);
+        return response;
+        // Log.i("mensaje2",macajena);
+    }
+    
+    @Override
+    public void run() {
+        String accion = "";
+        
+        try {
+            accion = dis.readUTF();
+            System.out.println(accion);
+            if (accion.contains(",\"opcionactualizar\":\"1\"")) {
+                //System.out.println(accion);
+                String cad = accion.replace(",\"opcionactualizar\":\"1\"", "");
+                // if (accion.equals("hola")) {
+                System.out.println("cliente " + this.socket + "dice " + cad);
+                String respuesta = actualizarNombre(cad);
+                //cad es la cadena en json para mandar a php "http://www.jimenezlepe.comuv.com/insertarnombre.php";
+
+                dos.writeUTF(respuesta);
+                //}
+            } else if (accion.equals("solicitaJSON")) {
+                System.out.println("cliente " + this.socket + "dice " + accion);
+                String respuesta = solicitarJSON();
+                //cad es la cadena en json para mandar a php "http://www.jimenezlepe.comuv.com/insertarnombre.php";
+
+                dos.writeUTF(respuesta);
+            } else if (accion.contains(",\"solicitapersona\":\"solicitapersona\"")) {
+                //System.out.println(accion);
+                String cad = accion.replace(",\"solicitapersona\":\"solicitapersona\"", "");
+                // if (accion.equals("hola")) {
+                System.out.println("cliente " + this.socket + "dice " + cad);
+                String respuesta = solicitarPersona(cad);
+                dos.writeUTF(respuesta);
+                //}
+            } else {
+                //inserción
+                String respuesta = insertar(accion);
+                System.out.println("Cliente " + this.socket + " dice" + accion);
+                if (respuesta != null) {
+                    dos.writeUTF(respuesta);
+                } else {
+                    dos.writeUTF("nada");
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        desconnectar();
+        
+    }
+}
---
+++ Proyectito_Sockets/UbicarPersonaSockets/app/src/main/java/com/example/edwin/ubicarpersona/dondeEsta.java
@@ -0,0 +1,238 @@
+package com.example.edwin.ubicarpersona;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+public class dondeEsta extends AppCompatActivity {
+    private static final String JSON_URL = "http://jimenezlepe.comuv.com/consultaubicacion.php";
+    String nombre="";
+    TextView t1;
+    protected DataOutputStream dos;
+    protected DataInputStream dis;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donde_esta);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        t1= (TextView) findViewById(R.id.textView);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras == null) {
+            nombre= null;
+        } else {
+            nombre= extras.getString("nombre");
+        Log.i("mensaje",nombre);
+        }
+
+        getJSON(JSON_URL);
+    }
+
+    public class SolicitarJSON extends AsyncTask<String, Void, String> {
+        ProgressDialog loading;
+
+        // String dstAddress="192.168.1.78";
+        String dstAddress="10.0.5.121";
+        int dstPort=5000;
+        String response = "";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            loading = ProgressDialog.show(dondeEsta.this, "Please Wait...", null, true, true);
+        }
+
+
+        @Override
+        protected String doInBackground(String... arg0) {
+
+            Socket socket = null;
+
+            try {
+                socket = new Socket(dstAddress, dstPort);
+                Log.i("mensaje", "conectado");
+                dos = new DataOutputStream(socket.getOutputStream());
+                dis = new DataInputStream(socket.getInputStream());
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("nombre", nombre);
+                    jsonObject.put("solicitapersona", "solicitapersona");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                String message = jsonObject.toString();
+
+                dos.writeUTF(message);
+
+                response = dis.readUTF();
+
+                Log.i("mensaje", response);
+                return response;
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+
+                response = "UnknownHostException: " + e.toString();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                response = "IOException: " + e.toString();
+            }finally{
+                if(socket != null){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            loading.dismiss();
+            procesarResultado(s);
+            //Log.i("mensaje", s);
+            //jsonresult = s;
+           // procesarResultado();
+
+        }
+
+    }
+
+
+    private void getJSON(String url) {
+        SolicitarJSON myClientTask= new SolicitarJSON();
+        myClientTask.execute();
+        /*
+        class GetJSON extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(dondeEsta.this, "Please Wait...", null, true, true);
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+                OutputStream os = null;
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("nombre", nombre);
+                    String message = jsonObject.toString();
+
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setReadTimeout( 10000 );
+                    con.setConnectTimeout( 15000 );
+                    con.setRequestMethod("POST");
+                    con.setDoInput(true);
+                    con.setDoOutput(true);
+                    con.setFixedLengthStreamingMode(message.getBytes().length);
+
+                    //make some HTTP header nicety
+                    con.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+                    con.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+
+                    //open
+                    con.connect();
+
+                    //setup send
+
+                    os = new BufferedOutputStream(con.getOutputStream());
+                    os.write(message.getBytes());
+                    //clean up
+                    os.flush();
+
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Log.i("mensaje", s);
+
+               procesarResultado(s);
+            }
+        }
+        GetJSON gj = new GetJSON();
+        gj.execute(url);
+*/
+        //Toast.makeText(getApplicationContext(),);
+    }
+
+
+    public void procesarResultado(String jsonresult) {
+        Toast.makeText(getApplicationContext(), jsonresult, Toast.LENGTH_LONG).show();
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(jsonresult);
+            JSONArray arr = obj.getJSONArray("result");
+           String cad="";
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject m = arr.getJSONObject(i);
+                cad+= m.getString("nombre")+" estuvo en "+m.getString("ubicacion") +"en la fecha "+m.getString("fecha");
+                //Toast.makeText(getApplicationContext(),arr.getString(i),Toast.LENGTH_LONG).show();
+                Log.i("mensaje", m.getString("nombre"));
+            }
+            t1.setText(cad);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
