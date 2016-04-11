b9010deb246708e785df27cbd3de203b055bfe61
lepe92 <eejl_1992@hotmail.com>
2016-04-10 21:37:55 -0500

    actualizando archivos en servidor
    los xml

--- Simulador2.0/src/simulador/Simulador.java
+++ Simulador2.0/src/simulador/Simulador.java
@@ -1,46 +1,122 @@
 package simulador;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
 
-public class Simulador implements Runnable
-{    
-    public static void main(String[] args) 
-    {
+public class Simulador implements Runnable {
+
+    public static void main(String[] args) {
         Ruta500 r500 = new Ruta500();
         Ruta602 r602 = new Ruta602();
         Ruta25 r25 = new Ruta25();
         Ruta300 r300 = new Ruta300();
         Ruta142 r142 = new Ruta142();
-        
+
         r500.start();
         r602.start();
         r25.start();
         r300.start();
         r142.start();
-        
-          (new Thread(new Simulador())).start();
-          
-          Runnable helloRunnable = new Runnable() {
-    public void run() {
-        System.out.println("notificando suscriptores");
-        r142.notificarSuscriptores();
-        r500.notificarSuscriptores();
-        r602.notificarSuscriptores();
-        r25.notificarSuscriptores();
-        r300.notificarSuscriptores();
-    }
-};
 
-ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-executor.scheduleAtFixedRate(helloRunnable, 0, 3, TimeUnit.SECONDS);
+        
+                actualizarEnServidor("mapa25.xml");
+                actualizarEnServidor("mapa142.xml");
+                actualizarEnServidor("mapa300.xml");
+                actualizarEnServidor("mapa500.xml");
+                actualizarEnServidor("mapa602.xml");
+                
+                actualizarEnServidor("paradas25.xml");
+                actualizarEnServidor("paradas142.xml");
+                actualizarEnServidor("paradas300.xml");
+                actualizarEnServidor("paradas500.xml");
+                actualizarEnServidor("paradas602.xml");
+                
+        (new Thread(new Simulador())).start();
+
+        Runnable helloRunnable = new Runnable() {
+            public void run() {
+                System.out.println("notificando suscriptores");
+                r142.notificarSuscriptores();
+                r500.notificarSuscriptores();
+                r602.notificarSuscriptores();
+                r25.notificarSuscriptores();
+                r300.notificarSuscriptores();
+                
+                actualizarEnServidor("data25.xml");
+                actualizarEnServidor("data142.xml");
+                actualizarEnServidor("data300.xml");
+                actualizarEnServidor("data500.xml");
+                actualizarEnServidor("data602.xml");                              
+            }
+        };
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(helloRunnable, 0, 3, TimeUnit.SECONDS);
     }
 
     public void run() {
         Server servidor = new Server(5000);
     }
-    
- }
+
+    public static void actualizarEnServidor(String file) {
+        String server = "jimenezlepe.comuv.com";
+        int port = 21;
+        String user = "a2811468";
+        String pass = "ornitorrinco8";
+
+        FTPClient ftpClient = new FTPClient();
+        try {
+
+            ftpClient.connect(server, port);
+            ftpClient.login(user, pass);
+            ftpClient.enterLocalPassiveMode();
+
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+            // APPROACH #2: uploads second file using an OutputStream
+            File secondLocalFile = new File("C:/wamp/www/Ubus/" + file);
+            String secondRemoteFile = "public_html/Ubus/Administrador/Choferes/" + file;
+            InputStream inputStream = new FileInputStream(secondLocalFile);
+
+            // ftpClient.deleteFile("public_html/Ubus/Administrador/info/data.xml);
+            System.out.println("Start uploading file");
+            OutputStream outputStream = ftpClient.storeFileStream(secondRemoteFile);
+            byte[] bytesIn = new byte[4096];
+            int read = 0;
+
+            while ((read = inputStream.read(bytesIn)) != -1) {
+                outputStream.write(bytesIn, 0, read);
+            }
+            inputStream.close();
+            outputStream.close();
+
+            boolean completed = ftpClient.completePendingCommand();
+            if (completed) {
+                System.out.println("The file is uploaded successfully.");
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
